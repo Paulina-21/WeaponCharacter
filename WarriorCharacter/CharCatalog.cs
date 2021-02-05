8652b356067c860bcc01ed4c2b3f45c5107a47f3
BananNakryvay <banannakryvay@gmail.com>
2021-02-05 13:13:22 +0100

    Update CharCatalog.cs

--- WarriorCharacter/CharCatalog.cs
+++ WarriorCharacter/CharCatalog.cs
@@ -11,17 +11,36 @@ namespace WarriorCharacter
         {
             D=new Dictionary<string, Character>();
         }
+        public void AddChar(Character character)
+        {
+            while(Search(character.Name) != null)
+            {
+                Console.WriteLine("This name already exist");
+                character.Name =  Console.ReadLine();
+            }
+            D.Add(character.Name, character);
+        }
+
+        public void RemoveChar(Character character)
+        {
+            while (Search(character.Name) == null)
+            {
+                Console.WriteLine("Wrong name, write correct one");
+                character.Name = Console.ReadLine();
+            }
+            D.Remove(character.Name);
+        }
+
+        public void UpdateChar(Character character)
+        {
+            if(Search(character.Name) != null) D[character.Name] = character;
+            Console.WriteLine("This name doesnt exist");
+        }
 
         public Character Search(string name)
         {
             if (D.ContainsKey(name)) return D[name];
             else return null;
         }
-
-        public void UpdateChar(Character character)
-        {
-            D[character.Name] = character;
-        }
-
     }
 }
