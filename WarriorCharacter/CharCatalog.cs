0c7ff7400c7e8c603cac679882263a91a6ce97c3
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-02-05 12:57:03 +0100

    CharCata

---
+++ WarriorCharacter/CharCatalog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarriorCharacter
+{
+    class CharCatalog
+    {
+        private Dictionary<string, Character> D;
+        public CharCatalog()
+        {
+            D=new Dictionary<string, Character>();
+        }
+
+    }
+}
