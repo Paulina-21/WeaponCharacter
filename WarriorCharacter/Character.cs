63174a10a2304fd84eb793e450f3fab024a100f6
Paula Catalina Curca <paul6465@edu.easj.dk>
2021-02-05 12:24:53 +0100

    Class Character

---
+++ WarriorCharacter/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarriorCharacter
+{
+    class Character
+    {
+        public Character(string n, string r, string cl, double hp)
+        {
+
+        }
+
+        public string Name { set; get; }
+        public string Race { set; get; }
+        public string Class { set; get; }
+        public double HP { set; get; }
+        public string WeaponName { set; get; }
+
+    }
+}
